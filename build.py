d564e6af59b9357a47e72b780390b0906391c0c3
darekarankita0 <55353280+darekarankita0@users.noreply.github.com>
2024-03-15 19:26:44 +0530

    Update and rename jenkinstest.py to build.py

---
+++ build.py
@@ -0,0 +1,9 @@
+# build.py
+
+def calculate_sum(numbers):
+    return sum(numbers)
+
+if __name__ == "__main__":
+    numbers = [1, 2, 3, 4, 5]
+    total_sum = calculate_sum(numbers)
+    print("Total sum:", total_sum)
